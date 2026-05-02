6e713029c9fa1837bc5181f885835e91474fb77e
Abhishek <abhishekkanichery@gmail.com>
2026-05-02 10:25:45 +0530

    feat: styled basic login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,26 @@
 const Login = () => {
   return (
-    <div className="absolute inset-0 z-10 w-3/12 h-1/2 flex flex-col gap-4 bg-black/60 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <h1>Sign In</h1>
-      <form action="" method="post">
-        <input type="text" placeholder="Email or phone number" className="" />
-        <input type="text" placeholder="Password" className="" />
-        <button type="submit" className="">
+    <div className="absolute inset-0 p-16 z-10 w-3/12 h-7/12 flex flex-col gap-4 justify-start items-start bg-black/60 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <h1 className="text-3xl font-bold w-full mb-6">Sign In</h1>
+      <form
+        action=""
+        method="post"
+        className="flex flex-col justify-center items-center gap-4 w-full"
+      >
+        <input
+          type="text"
+          placeholder="Email or phone number"
+          className="w-full bg-surface-2 p-4 rounded-sm"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="w-full bg-surface-2 p-4 rounded-sm"
+        />
+        <button
+          type="submit"
+          className="w-full mt-6 bg-primary p-4 cursor-pointer hover:bg-primary-dark font-medium"
+        >
           Sign In
         </button>
       </form>
