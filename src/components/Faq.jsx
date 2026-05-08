29ec1d1dcb343f34f3811a23855c5a51a31a4782
Abhishek <abhishekkanichery@gmail.com>
2026-05-08 13:26:09 +0530

    fix: added toggle option

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,6 +1,11 @@
+import { PlusIcon } from "@phosphor-icons/react";
 import { FAQ_DUMMY } from "../utils/dummyData";
 
 const Faq = () => {
+  const toggleItem = (item) => {
+    console.log(item);
+  };
+
   return (
     <div className="faq">
       <h1 className="text-2xl font-semibold mb-4">
@@ -10,9 +15,12 @@ const Faq = () => {
         {
           <ul className="flex flex-col gap-2">
             {FAQ_DUMMY.map((i) => (
-              <li className="px-6 py-7 bg-surface-2 text-2xl cursor-pointer hover:bg-border">
-                {i.question}
-              </li>
+              <button onClick={() => toggleItem(i)}>
+                <li className="px-6 py-7 bg-surface-2 text-2xl cursor-pointer hover:bg-border flex justify-between items-center">
+                  {i.question}
+                  <PlusIcon size={32} />
+                </li>
+              </button>
             ))}
           </ul>
         }
