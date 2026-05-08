e5398ddb57e0339e649427979cab3d6b3c235b61
Abhishek <abhishekkanichery@gmail.com>
2026-05-08 13:16:14 +0530

    feat: added faq list

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,7 +1,22 @@
+import { FAQ_DUMMY } from "../utils/dummyData";
+
 const Faq = () => {
   return (
     <div className="faq">
-      <h1>Frequently Asked Questions</h1>
+      <h1 className="text-2xl font-semibold mb-4">
+        Frequently Asked Questions
+      </h1>
+      <div className="">
+        {
+          <ul className="flex flex-col gap-2">
+            {FAQ_DUMMY.map((i) => (
+              <li className="px-6 py-7 bg-surface-2 text-2xl cursor-pointer hover:bg-border">
+                {i.question}
+              </li>
+            ))}
+          </ul>
+        }
+      </div>
     </div>
   );
 };
