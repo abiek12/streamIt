dc1131ce217ef4e985311fbd617738493bab2c78
Abhishek <abhishekkanichery@gmail.com>
2026-05-13 22:23:33 +0530

    add authenticated users in to global state store

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import { RouterProvider } from "react-router-dom";
 import appRouter from "./routes/AppRouter";
 import { ToastContainer } from "react-toastify";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import appStore from "./stores/appStore";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { addUser, removeUser } from "./stores/userSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid, email, displayName }));
+        dis;
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+      }
+    });
+  }, []);
+
   return (
     <Provider store={appStore}>
       <div>
