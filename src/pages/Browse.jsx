aefe4e9f30ceaea062c9761db4f32874cef8afb9
Abhishek <abhishekkanichery@gmail.com>
2026-05-14 07:41:23 +0530

    added header on browser page

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,6 +1,9 @@
+import Header from "../components/Header";
+
 const Browse = () => {
   return (
     <div className="Browse">
+      <Header />
       <h1 className="text-white">Browse Page</h1>
     </div>
   );
