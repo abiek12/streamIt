250977b2a1b0c6f916ae9afef533f9bf7106fadf
Abhishek <abhishekkanichery@gmail.com>
2026-05-16 11:22:43 +0530

    added tmdb api config

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,10 @@
 export const CDN_URL = "https://occ-0-4994-3662.1.nflxso.net/dnm/api/v6";
+
+export const TMDB_URL = "https://api.themoviedb.org/3/authentication";
+export const TMDB_API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${import.meta.env.TMDB_ACCESS_TOKEN}`,
+  },
+};
