9bfe4a3d4050c2db28740a41cb1f78f9c5e8e1aa
Abhishek <abhishekkanichery@gmail.com>
2026-05-20 22:16:21 +0530

    fix: form reset in auth

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -33,6 +33,7 @@ const Auth = () => {
       signInWithEmailAndPassword(auth, data.email, data.password)
         .then((user) => {
           console.log("Login successfull!");
+          reset();
           popupNotification(TOAST_TYPE.SUCCESS, "Logged in successfully!");
         })
         .catch((error) => {
@@ -47,6 +48,7 @@ const Auth = () => {
       createUserWithEmailAndPassword(auth, data.email, data.password)
         .then((userCred) => {
           console.log("Registered successfully!");
+          reset();
           popupNotification(TOAST_TYPE.SUCCESS, "Registered successfully!");
         })
         .catch((error) => {
@@ -57,14 +59,13 @@ const Auth = () => {
           setIsLoading(false);
         });
     }
-    reset();
   };
 
-  useEffect(() => {
-    if (isSubmitSuccessful) {
-      reset();
-    }
-  }, [isSubmitSuccessful, reset]);
+  // useEffect(() => {
+  //   if (isSubmitSuccessful) {
+  //     // reset();
+  //   }
+  // }, [isSubmitSuccessful, reset]);
 
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black/70 w-full max-w-md p-16 rounded-md flex flex-col gap-4">
