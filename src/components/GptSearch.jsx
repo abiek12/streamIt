8ae38708f39cd59e66de0a794c14790f63d242e6
Abhishek <abhishekkanichery@gmail.com>
2026-05-24 13:06:01 +0530

    refc: change to search bar and result list into seperate components

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -1,15 +1,14 @@
+import GptSearchBar from "./GptSearchBar";
+import GptSearchResults from "./GptSearchResults";
+
 const GptSearch = () => {
   return (
     <div>
-      <div className="relative min-h-screen pb-20 overflow-hidden bg-black my-36 px-36">
-        <h1 className="text-2xl font-semibold text-white">
-          Search your queries
-        </h1>
-        <input type="text" name="" id="" />
+      <div className="relative min-h-screen pb-20 overflow-hidden bg-black my-36 px-36 text-white flex flex-col gap-4">
+        <h1 className="text-2xl font-semibold">Search your queries</h1>
+        <GptSearchBar />
       </div>
-      <div className="results">
-        <p>Movie results will be shown here</p>
-      </div>
+      <GptSearchResults />
     </div>
   );
 };
---
+++ src/components/GptSearchBar.jsx
@@ -0,0 +1,17 @@
+const GptSearchBar = () => {
+  return (
+    <div className="flex gap-4 justify-between items-center w-full">
+      <form action="" method="post">
+        <input
+          className="border border-solid border-white"
+          type="text"
+          name=""
+          id=""
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default GptSearchBar;
