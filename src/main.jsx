050a2e1159ea97ea86cb260dc702535a25bbe4ce
Abhishek <abhishekkanichery@gmail.com>
2026-05-28 21:41:05 +0530

    remove unwanted packages in i18n

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
 import "./utils/i18n.js";
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
