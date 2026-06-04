a20929f9c578cd1f7f668b02b7d18f2cfbcc8914
Abhishek <abhishekkanichery@gmail.com>
2026-06-04 08:23:33 +0530

    fix: translation typo

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -3,12 +3,12 @@ import { initReactI18next } from "react-i18next";
 
 const resources = {
   en: {
-    tranlation: {
+    translation: {
       welcomeMessage: "Hey Abhishek welcome to streamIt",
     },
   },
   hi: {
-    tranlation: {
+    translation: {
       welcomeMessage: "हे अभिषेक, स्ट्रीमइट में आपका स्वागत है।",
     },
   },
@@ -20,7 +20,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources,
-    lng: "en",
+    lng: "hi",
     debug: true,
 
     interpolation: {
