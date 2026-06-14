023f94d94a35f56b01a34eff786925a41a4ad81b
Abhishek <abhishekkanichery@gmail.com>
2026-06-14 17:27:38 +0530

    added support for localization in search page

--- src/components/GptSearchResults.jsx
+++ src/components/GptSearchResults.jsx
@@ -1,10 +1,15 @@
+import { useTranslation } from "react-i18next";
 import MovieList from "./MoiveList";
 
 const GptSearchResults = ({ recommendedMovies, movieTitles }) => {
+  const { t } = useTranslation();
+
   if (!recommendedMovies || !movieTitles) return null;
   return (
     <div className="flex flex-col gap-6">
-      <h1 className="text-2xl font-semibold text-white">Search results</h1>
+      <h1 className="text-2xl font-semibold text-white">
+        {t("gptSearch.subTitle")}
+      </h1>
       {movieTitles.map((i, idx) =>
         recommendedMovies[idx].length > 0 ? (
           <MovieList title={i} movies={recommendedMovies[idx]} />
