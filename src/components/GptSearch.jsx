5495f04c55f629eab49f35f439e858fdf256551d
Abhishek <abhishekkanichery@gmail.com>
2026-06-14 11:26:49 +0530

    fix styling issue

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -10,12 +10,12 @@ const GptSearch = () => {
   );
 
   return (
-    <div>
-      <div className="relative min-h-screen pb-20 overflow-hidden bg-black my-36 px-36 text-white flex flex-col gap-4">
+    <div className="flex flex-col gap-16">
+      <div className="relative overflow-hidden bg-black mt-36 px-36 text-white flex flex-col gap-4">
         <h1 className="text-2xl font-semibold">{t("gptSearch.title")}</h1>
         <GptSearchBar />
       </div>
-      <div className="my-36 px-36">
+      <div className="px-36">
         <GptSearchResults
           recommendedMovies={recommendedMoviesResults}
           movieTitles={gptRecommendedMovies}
--- src/components/GptSearchResults.jsx
+++ src/components/GptSearchResults.jsx
@@ -3,7 +3,7 @@ import MovieList from "./MoiveList";
 const GptSearchResults = ({ recommendedMovies, movieTitles }) => {
   if (!recommendedMovies || !movieTitles) return null;
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-6">
       <h1 className="text-2xl font-semibold text-white">Search results</h1>
       {movieTitles.map((i, idx) => (
         <MovieList title={i} movies={recommendedMovies[idx]} />
